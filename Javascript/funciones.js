3e6c62aa5d27ca35be0984ee78e6ff78ff14ab88
pilarcsa <pilarcsa.99@gmail.com>
2025-01-14 20:33:57 +0100

    funcionamiento del botón del modal start

--- Javascript/funciones.js
+++ Javascript/funciones.js
@@ -2,20 +2,23 @@ const tablero = document.querySelector(".tablero-juego");
 const elemPuntuacion = document.querySelector(".puntaje");
 const elemPuntuacionMaxima = document.querySelector(".puntaje-alto");
 const modal = document.querySelector(".modal");
-const botonModal = document.querySelector (".modal button");
+const botonModal = document.querySelector(".modal button");
 const textoModal = document.querySelector (".modal p");
-let tiempoInicial =  10;
+const contTiempo = document.querySelector (".tiempo");
 
+let tiempoInicial =  59;
+let tiempo = null;
 let gameOver = false;
-let timer = null;
 let topoX, topoY;
 let falsotopoX, falsotopoY;
+let bombaX,bombaY;
 
 
-//¿cÓMO HACER APÀRECER UNOS topos EN LOS HUECO DEL GRID?
+
+
 let setIntervalId;
 let puntuacion = 0;
- ////////REVISAR ELEMENTOS DE POUNTUCIÓN Y PUNTUACIÓN ALTA!!!!!!
+
  let puntuacionMaxima = localStorage.getItem("puntaje-alto") || 0;
  elemPuntuacionMaxima.innerHTML = `High Score: ${puntuacionMaxima}`;
  
@@ -24,8 +27,8 @@ const topoPosicion = () => {
     do{   topoX= Math.floor ( Math.random() *10) +1;
             topoY= Math.floor ( Math.random() *10) +1;
     }while 
-        (topoX === bombaX && topoY === bombaY)
-        (topoX === falsotopoX && topoY === falsotopoX);
+        (topoX === bombaX && topoY === bombaY)//el do y while lo he mirado en www3school y ChatGpt contrastando ambos contenidos
+        (topoX === falsotopoX && topoY === falsotopoX);//la sintaxis de este código se me escapaba a lo que habiamos dado en clase por ello lo recogí de Chat gpt 
 };
 
 const falsotopoPosicion = () => {
@@ -43,45 +46,34 @@ const bombaPosicion = () => {
 
 
 
-const GameOverAlerta = () => {
+const gameOverModal = () => {
     clearInterval(setIntervalId);
-    alert("Game Over!");
+    textoModal.innerText = "perdí";
+    console.log("....perdí =(");
+    modal;
     location.reload();
 }
+if(gameOver){gameOverModal()};
 
 //inicio del tiempo y modales
-function jugar(tiempo){
-    
-    puntuacion = 0;
-    contenedorTimer.innerHTML = "";
 
-    for(let i = 0; i < tiempo; i++){
-        let cuadro = document.createElement("div");
-        cuadro.style.width = `calc(${100/tiempo}% - 20px)`;
-        contenedorTimer.appendChild(cuadro);
-    }
 
-     timer = setInterval(() => {
-        contenedorTimer.children[0].remove();
-        
-        if(contenedorTimer.children.length == 0){
-            clearInterval(timer);
-            modal.classList.remove("desaparece");
-            textoModal.innerText = "perdí";
-            console.log("....perdí =(");
-            gameOver = true;
-        }
-    },1000);
-    //inicio del juego
-}
+
+//inicio del juego
+
 const inicio = () => {
-    if(gameOver) return GameOverAlerta();
+
 
     
+
+    // esto lo he recogido del juego de los círculos que realizamos en clase(aun no lo he escrito)
+
+
     let Marcador = `<div id="topoClick"class="topo" style="grid-area: ${topoY} / ${topoX}"><img src="img/1459443.png" alt=""></div> <div id="falsoTopoClick"class="topo" style="grid-area: ${falsotopoY} / ${falsotopoX}"><img src="img/1459520.png" alt=""></div><div id="bombaClick"class="topo" style="grid-area: ${bombaY} / ${bombaX}"><img src="img/595582.png" alt=""></div>`;
-
-    
     tablero.innerHTML = Marcador;
+
+    //he solucioando la cuestion dde clickear el topo y que cuente como puntuación buscando en w3school las formas de utilizar el addeventlistener 
+    //la solucion final es crear un div desde javascript con la variable "Marcador", y una vez creada llamar a ese mismo div en html mediante el id correspondiente.
     document.getElementById("topoClick").addEventListener("click", function() {
         topoPosicion();
         puntuacion+= 1;
@@ -89,7 +81,7 @@ const inicio = () => {
         puntuacionMaxima = puntuacion >= puntuacionMaxima ? puntuacion : puntuacionMaxima;
         localStorage.setItem("puntaje-alto", puntuacionMaxima);
         elemPuntuacionMaxima.innerHTML = `High Score: ${puntuacionMaxima}`;
-        //esto lo he buscado con el Chat Gpt yt con ayuda de videotutoriales.
+        //la Score y el Highscore lo he buscado con el Chat Gpt y con ayuda de un videotutorial ya que esta parte la veía complicada, sobretodo colocar la High-Score.
       });
 
       document.getElementById("falsoTopoClick").addEventListener("click", function() {
@@ -101,9 +93,18 @@ const inicio = () => {
 
       document.getElementById("bombaClick").addEventListener("click", function() {
         gameOver = true;
-     
+        gameOverModal();
       });
 };
+
+botonModal.addEventListener("click", () => {
+  const quitarModal = document.getElementById("quitarModal");
+  quitarModal.remove();//función del botón start
+  setIntervalId = setInterval(inicio, 125);//aplicar el idi del inicio para comenzar el juego
+  
+ });
+
+
 
 bombaPosicion();
 falsotopoPosicion();
@@ -112,7 +113,6 @@ setInterval(bombaPosicion, 1180);
 setInterval(falsotopoPosicion, 1100);
 setInterval(topoPosicion, 1000);
 //inicio();
-setIntervalId = setInterval(inicio, 125);
 
 
 
