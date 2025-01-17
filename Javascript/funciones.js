26e8f92ff48fd91cec4e3493226b04879d664098
pilarcsa <pilarcsa.99@gmail.com>
2025-01-17 12:12:24 +0100

    Rectificación de HTML y simplificación de javascript

--- Javascript/funciones.js
+++ Javascript/funciones.js
@@ -73,11 +73,11 @@ const bombaPosicion = () => {
 
 };
 
+//////HACER EL MODAL DE PERDERRRR!!!!!
 
 //modal de perder
 
  gameOverModal = () => {
-  botonModal();
   textoModal.innerText = `Puntos totales: ${puntuacionMaxima}`;
   clearInterval(setIntervalId); 
 }
@@ -88,7 +88,7 @@ const bombaPosicion = () => {
 function inicioContador() {
   const divCuentaAtras = document.getElementById("divCuentaAtras");
 
-  divCuentaAtras.style.animation = "reducirBarra 5s linear forwards";
+  divCuentaAtras.style.animation = "reducirBarra 60s linear forwards";
 
   divCuentaAtras.addEventListener("animationend", () => {
     gameOver = true; 
@@ -96,63 +96,27 @@ function inicioContador() {
   });
 }
 
-const inicio = () => {
+function inicio(){
 
     gameOver = false;
 
     inicioContador();
 
-    const imgtopo = "img/topo.png";
-    const imgtopom = imgtopo.replace("img/topo.png", "img/topom.png");
+   let imgtopo = "topo.webp";
 
-    const imgtopox2 = "img/topox2.png";
-    const imgtopox2m = imgtopox2.replace("img/topox2.png", "img/topox2m.png");
+    let imgtopox2 = "topox2.webp";
 
-    const imgbombatopo = "img/topobomba.png";
-    const imgbombatopom = imgbombatopo.replace("img/topobomba.png", "img/topobombam.png");
+    let imgbombatopo = "topobomba.webp";
+   
 
-    // esto lo he recogido de chat GPT y lo he adaptado a mi código, me habia quedado pillada en la forma de rremplazar ua imgen por otra, 
-    //<img id="topoImg" src="${imgtopo}" alt=""> esta solución de Chat Gpt me ha hecho ver que tengo muchas mas posibilidades de las pensaba para jugar con lo elementos incluso si aún no se han creado o se crean insitu en el inicio del juego.
-
-
-    let Marcador = 
-    `<div id="topoClick" class="topo" style="grid-area: ${topoY} / ${topoX}">
-      <img id="topoImg" src="${imgtopo}" alt="">
-    </div> 
-    <div id="Topox2Click" class="topo" style="grid-area: ${Topox2Y} / ${Topox2X}">
-      <img id="topoX2Img" src="${imgtopox2}" alt="">
-    </div>
-    <div id="bombaClick" class="topo" style="grid-area: ${bombaY} / ${bombaX}">
-      <img id="topoBImg" src="${imgbombatopo}" alt="">
-    </div> 
-
-    <img src="img/tierra.png" alt="">
-    <img src="img/tierra.png" alt="">
-    <img src="img/tierra.png" alt="">
-    <img src="img/tierra.png" alt="">
-    <img src="img/tierra.png" alt="">
-    <img src="img/tierra.png" alt="">
-    <img src="img/tierra.png" alt="">
-    <img src="img/tierra.png" alt="">
-    <img src="img/tierra.png" alt="">
-    <img src="img/tierra.png" alt="">
-    <img src="img/tierra.png" alt="">
-    <img src="img/tierra.png" alt="">
-    <img src="img/tierra.png" alt="">`;
-    //el Marcador lo saqué de un tutorial del juego snake, que se usaba igual para la manzana.
-    //lo de la img de la tierra lo he hecho viendo que al meter el marcador me eliminaba del HTML los img que puse e introducia los del Marcador
-
-
-    
-
-
-    tablero.innerHTML = Marcador;
+  
 
     //he solucioando la cuestion dde clickear el topo y que cuente como puntuación buscando en w3school las formas de utilizar el addeventlistener 
-    //la solucion final es crear un div desde javascript con la variable "Marcador", y una vez creada llamar a ese mismo div en html mediante el id correspondiente.
-    document.getElementById("topoClick").addEventListener("click", function() {
- 
+    //la solucion final es crear los divs en html llamar a ese mismo div en html mediante el id correspondiente.
+  
       const topoImg = document.getElementById("topoImg");
+
+      topoImg.addEventListener("click", function() {
       topoImg.src = imgtopom; 
         puntuacion+= 15;
         elemPuntuacion.innerHTML = `Score: ${puntuacion}`;
@@ -160,26 +124,33 @@ const inicio = () => {
         localStorage.setItem("puntaje-alto", puntuacionMaxima);
         elemPuntuacionMaxima.innerHTML = `High Score: ${puntuacionMaxima}`;
         //la Score y el Highscore lo he buscado con el Chat Gpt y con ayuda de un videotutorial ya que esta parte la veía complicada, sobretodo colocar la High-Score.
+        console.log("click topo");
       });
 
-      document.getElementById("Topox2Click").addEventListener("click", function() {
+      
       const topoX2Img = document.getElementById("topoX2Img");
+      
+      topoX2Img.addEventListener("click", function() {
       topoX2Img.src = imgtopox2m; 
         puntuacion+= 30;
         elemPuntuacion.innerHTML = `Score: ${puntuacion}`;
         puntuacionMaxima = puntuacion >= puntuacionMaxima ? puntuacion : puntuacionMaxima;
         localStorage.setItem("puntaje-alto", puntuacionMaxima);
         elemPuntuacionMaxima.innerHTML = `High Score: ${puntuacionMaxima}`;
-    
-      });
+        console.log("click topoX2");
+      }); 
+      
+      
+       
+        const topoBImg = document.getElementById("topoBImg");
 
-      document.getElementById("bombaClick").addEventListener("click", function() {
-        const topobombaImg = document.getElementById("topoBImg");
-        topobombaImg.src = imgbombatopom; 
+        topoBImg.addEventListener("click", function() {
+        topoBImg.src = imgbombatopom; 
         puntuacion -= 15, 
         vidas -= 1;
-         elemPuntuacion.innerHTML = `Score: ${puntuacion}`
-         elemVidas.innerHTML = `🤍: ${vidas}`;
+        elemPuntuacion.innerHTML = `Score: ${puntuacion}`
+        elemVidas.innerHTML = `🤍: ${vidas}`;
+        console.log("click bomba");
 
       if(vidas == 0){
         gameOver = true;
@@ -189,6 +160,8 @@ const inicio = () => {
     
     });
 };
+
+
 
 botonModal.addEventListener("click", () => {
   quitarModal.remove();//función del botón start
@@ -201,9 +174,9 @@ botonModal.addEventListener("click", () => {
 bombaPosicion();
 Topox2Posición();
 topoPosicion();
-setInterval(bombaPosicion, 1000);
-setInterval(Topox2Posición, 1000);
-setInterval(topoPosicion, 1000);
+setInterval(bombaPosicion, 2000);
+setInterval(Topox2Posición, 2000);
+setInterval(topoPosicion, 2000);
 //inicio();
 
 
